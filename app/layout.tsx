cfa8de9520f6a065de83ab58fd033aaf856c6e3c
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-04-20 01:21:21 +0530

    feat: Updated Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,37 +2,39 @@ import type { Metadata } from "next";
 import { Josefin_Sans, Cinzel } from "next/font/google";
 import "./globals.css";
 import { Footer, Header } from "@/components";
+import { Sidebar } from "@/components/Sidebar";
 
 const josefin = Josefin_Sans({
-  variable: "--font-josefin",
-  subsets: ["latin"],
+	variable: "--font-josefin",
+	subsets: ["latin"],
 });
 
 const cinzel = Cinzel({
-  variable: "--font-cinzel",
-  subsets: ["latin"],
+	variable: "--font-cinzel",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Suryakant | Portfolio",
-  description: "Hi, I'm Suryakant. I am a Frontend Developer. this is my portfolio. where you can explore my projects, blogs and skills and my learning journey.",
+	title: "Suryakant | Portfolio",
+	description:
+		"Hi, I'm Suryakant. I am a Frontend Developer. this is my portfolio. where you can explore my projects, blogs and skills and my learning journey.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${josefin.variable} ${cinzel.variable} antialiased`}
-      >
-        <Header />
-        
-        {children}
-        {/* <Footer /> */}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${josefin.variable} ${cinzel.variable} antialiased`}>
+				<Header />
+				<Sidebar />
+				<div className="mt-15 lg:mt-0 lg:ml-65">
+					<div className="p-4">{children}</div>
+					<Footer />
+				</div>
+			</body>
+		</html>
+	);
 }
---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,5 @@
+export const Sidebar = () => {
+  return (
+    <div className="hidden w-65 h-screen fixed left-0 bottom-0 lg:flex flex-col border-r border-secondary/10">Sidebar</div>
+  )
+}

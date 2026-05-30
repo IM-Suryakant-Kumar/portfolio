ed348547dd2d92323e61e84fe0a4e5edc223f37d
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-05-30 16:14:56 +0530

    feat: new planning

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,5 +1,13 @@
+import { ThemeToggle } from "../Header";
+
 export const Sidebar = () => {
-  return (
-    <div className="hidden w-65 h-screen fixed left-0 bottom-0 lg:flex flex-col border-r border-secondary/10">Sidebar</div>
-  )
-}
+	return (
+		<div className="hidden w-65 xl:w-80 h-screen fixed left-0 bottom-0 lg:flex flex-col border-r border-secondary/10">
+			<div className="w-65 ml-auto p-4">
+				<div className="p-4 border-b border-secondary/10"><ThemeToggle /></div>
+				<div className="p-4 border-b border-secondary/10">div</div>
+				<div className="p-4 border-b border-secondary/10">div</div>
+			</div>
+		</div>
+	);
+};

c7857fd9a4ffbc76183c7d3cd79cccfed412b252
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-06-13 17:02:42 +0530

    feat: Updated sidebar

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 A personal portfolio to keep track of my journey.
 
 ## Features
-- [ ] Navbar
-- [ ] Sidebar
-- [ ] Home Page
-- [ ] Projects Page
-- [ ] Blogs Page
-- [ ] About Page
-- [ ] Footer
+- Navbar
+- Sidebar
+- Home Page
+- Projects Page
+- Blogs Page
+- About Page
+- Footer
 
 ## Tech Stack
 - Next.js
-- Tailwindcss
+- Tailwindcss 
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -4,8 +4,21 @@ export const Sidebar = () => {
 	return (
 		<div className="hidden w-65 xl:w-80 h-screen fixed left-0 bottom-0 lg:flex flex-col border-r border-secondary/10">
 			<div className="w-65 ml-auto p-4">
-				<div className="p-4 border-b border-secondary/10"><ThemeToggle /></div>
-				<div className="p-4 border-b border-secondary/10">div</div>
+				<div className="p-4 border-b border-secondary/10">
+					<ThemeToggle />
+				</div>
+				<div className="p-4 border-b border-secondary/10">
+					<h2 className="text-secondary text-lg font-bold">About me</h2>
+					<p className="text-gray-400">
+						I&apos;m{" "}
+						<span className="span-primary font-bold underline decoration-dotted text-orange-500 font-cinzel">
+							Suryakant
+						</span>
+						, Full Stack Web Developer.
+					</p>
+				</div>
+				<div className="p-4 border-b border-secondary/10">
+				</div>
 				<div className="p-4 border-b border-secondary/10">div</div>
 			</div>
 		</div>
